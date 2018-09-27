c22eb92ed004c1469c7e98a88ff770f90205ee07
rajat.saluja12 <er.rajatsaluja@gmail.com>
2018-09-27 08:20:00 +0530

    Changes Made

--- src/test/java/rent/checker.java
+++ src/test/java/rent/checker.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class checker {
 	@Test
 	public  void methodnew11() {
-		System.out.println("Check the sign changes happy enough");
+		System.out.println("Check the sign changes happy changes ");
 
 	}
 
